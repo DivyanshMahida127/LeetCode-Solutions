f7bdbb1f0e1dff1b14b5dfb8508c5d35c5ec932e
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-05-21 12:39:44 +0530

    Time: 2 ms (60.53%), Space: 8.3 MB (25.23%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -2,18 +2,37 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        for(int i=0;i<n-1;i++){
-            int key = nums[i];
-            int j = i+1;
-            while(j<n){
-                if(nums[j] < key){
-                    int temp = key;
-                    nums[i] = nums[j];
-                    key = nums[j];
-                    nums[j] = temp;
-                }
-                j++;
+        int a,b,c;
+        a=0,b=0,c=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                a++;
             }
+            else if(nums[i] == 1){
+                b++;
+            }
+            else{
+                c++;
+            }
+        }
+        
+        int j=0;
+        while(a != 0){
+            nums[j] = 0;
+            a--;
+            j++;
+        }
+        
+        while(b != 0){
+            nums[j] = 1;
+            b--;
+            j++;
+        }
+        
+        while(c != 0){
+            nums[j] = 2;
+            c--;
+            j++;
         }
     }
 };
