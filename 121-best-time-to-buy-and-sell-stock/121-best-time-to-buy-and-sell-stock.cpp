85c5717055e755926aa66fd54b2d861639f33cb2
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-05-25 17:44:18 +0530

    Time: 152 ms (70.07%), Space: 93.4 MB (53.58%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n == 1){
+            return 0;
+        }
+        else if(n == 2){
+            if(prices[1] >= prices[0]){
+                return prices[1]-prices[0];
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            int answer = INT_MIN;
+            int result;
+            int i,j;
+            j = 0;
+            for(i=1;i<n;i++){
+                result = prices[i] - prices[j];
+                answer = max(answer,result);
+                
+                if(result <= 0){
+                    j=i;
+                }
+                // cout << i << " " << result << "\n";
+            }
+            
+            if(answer <= 0){
+                return 0;
+            }
+            else{
+                return answer;
+            }
+        }
+        // return answer;
+    }
+};
