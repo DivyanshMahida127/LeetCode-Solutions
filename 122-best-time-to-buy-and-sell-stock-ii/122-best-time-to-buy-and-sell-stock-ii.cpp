f51a6f7e14eb3de859d61fee5abba6f490bb6428
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-06-01 18:08:04 +0530

    Time: 9 ms (56.58%), Space: 13 MB (88.46%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,46 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        if(n == 1){
-            return 0;
-        }
-        else if(n == 2){
-            if(prices[0] < prices[1]){
-                return prices[1]-prices[0];
-            }
-            else{
-                return 0;
+        int length;
+        length = prices.size();
+        int answer;
+        answer = 0;
+        for(int i=1;i<length;i++){
+            if((prices[i] - prices[i-1]) > 0){
+                answer += prices[i] - prices[i-1];
             }
         }
-        else{
-            int i,j;
-            int result, answer,tillnow;
-            answer = 0;
-            i=0;
-            while(i<n){
-                j=i+1;
-                result = 0;
-                tillnow = 0;
-                while(j < n){
-                    result = prices[j] - prices[i];
-                    if(prices[j] > prices[i] && result > tillnow){
-                        tillnow = result;
-                    }
-                    else{
-                        break;
-                    }
-                    j++;
-                }
-                answer = answer + tillnow;
-                i=j;
-            }
-            if(answer <= 0){
-                return 0;
-            }
-            else{
-                return answer;
-            }
-        }
+        return answer;
     }
 };
