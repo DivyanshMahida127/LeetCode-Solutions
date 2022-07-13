998b3af4cae01f390dfcd0bceb1fc4c681b83271
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-07-14 02:32:48 +0530

    Time: 3 ms (90.56%), Space: 6.7 MB (99.46%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        vector<char> a(26,0);
+        int i,j,m,n,b;
+        m = key.length(), n = message.length();
+        
+        j = 0;
+        int count = 0;
+        for(i=0;i<m;i++){
+            b = key[i]-97;
+            if(b >= 0 && b <= 25){
+                if(a[b] == 0){
+                    a[b] = j + 97;
+                    j++;
+                }
+                
+                if(j == 26){
+                    break;
+                }
+            }
+        }
+        
+        // for(auto i:a){
+        //     cout << i << " ";
+        // }
+        // cout << "\n";
+        
+        string answer;
+        string c;
+        for(i=0;i<n;i++){
+            if(message[i] == ' '){
+                answer += " ";
+            }
+            else{
+                b = message[i]-97;
+                // cout << b << "\n";
+                c = a[b];
+                // cout << c << "\n";
+                answer += c;
+            }
+        }
+        return answer;
+    }
+};
