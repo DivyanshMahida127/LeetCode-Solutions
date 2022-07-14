c18d90109815f79804a8c2a509fde174ef01ac53
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-07-14 23:20:10 +0530

    Time: 24 ms (39.07%), Space: 13.5 MB (29.94%) - LeetHub

---
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double answer,a,b,c;
+        c = 100;
+        if(income == 0){
+            answer = 0;
+            return answer;
+        }
+        else{
+            answer = 0;
+            int i,j;
+            i=0;
+            a = brackets[0][0];
+            b = brackets[0][1];
+            if(a < income){
+                answer += ((a*b)/c);
+                income -= a;
+            }
+            else{
+                answer += ((income*b)/c);
+                return answer;
+            }
+            
+            i=1;
+            while(income >= 0){
+                a = brackets[i][0] - brackets[i-1][0];
+                b = brackets[i][1];
+                // cout << i << " " << a << " " << income << " " << answer << "\n";
+                if(a < income){
+                    answer += ((a*b)/c);
+                    income -= a;
+                }
+                else{
+                    answer += ((income*b)/c);
+                    return answer;
+                }
+                
+                
+                i++;
+            }
+        }
+        return answer;
+    }
+};
