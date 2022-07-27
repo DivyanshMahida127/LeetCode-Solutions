999cdc014d5935b2519ccbbb4ce82e45b797647b
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-07-27 20:38:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (88.98%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int largestInteger(int num) {
+        if(num <= 101){
+            return num;
+        }
+        vector<int> odd,even;
+        int i,j,remainder,m,count;
+        m = num;
+        count = 0;
+        while(m != 0){
+            remainder = m%10;
+            if(remainder%2 == 0){
+                even.push_back(remainder);
+            }
+            else{
+                odd.push_back(remainder);
+            }
+            count++;
+            m /= 10;
+        }
+        
+        sort(even.begin(),even.end());
+        sort(odd.begin(),odd.end());
+
+        i = 0;
+        j = 0;
+        m = num;
+        vector<int> result;
+        while(m != 0){
+            remainder = m%10;
+            if(remainder%2 == 0){
+                result.push_back(even[i]);
+                i++;
+            }
+            else{
+                result.push_back(odd[j]);
+                j++;
+            }
+            m/=10;
+        }
+        
+        // for(auto i:result){
+        //     cout << i << "\n";
+        // }
+        reverse(result.begin(),result.end());
+        int answer = 0;
+        for(i=0;i<result.size();i++){
+            answer = answer*10+result[i];
+            // cout << answer << "\n";
+        }
+        return answer;
+    }
+};
