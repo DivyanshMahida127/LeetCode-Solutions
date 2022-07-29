d24a947e0478e39bfebfb75b2372089f5a13a517
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-07-29 14:23:47 +0530

    Time: 10 ms (77.60%), Space: 10.4 MB (98.99%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int bs(int start,int end, int original,vector<int> &nums){
         int mid = (start+end)/2;
-        // cout << mid << " " << original << "\n";
         if(start<=end){
             if(nums[mid] < original){
                 return bs(mid+1,end,original,nums);
@@ -12,7 +11,6 @@ public:
             }
             else if(nums[mid] == original){
                 original = original*2;
-                // cout << original <<  "\n";
                 return bs(0,nums.size()-1,original,nums);
             }
         }
@@ -21,8 +19,7 @@ public:
     
     int findFinalValue(vector<int>& nums, int original) {
         sort(nums.begin(),nums.end());
-        int n;
-        n = nums.size();
+        int n = nums.size();
         int start = 0,end = n-1;
         int answer = bs(start,end,original,nums);
         return answer;
