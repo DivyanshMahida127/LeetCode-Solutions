5343b651b68a9c023ad6284183c13a9fbfc293b1
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-08-01 17:09:40 +0530

    Time: 3 ms (87.00%), Space: 8 MB (34.23%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -3,11 +3,11 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> answer;
         string a,b,c;
-        int d;
+        int i,d;
         a = "FizzBuzz";
         b = "Fizz";
         c = "Buzz";
-        for(int i=0;i<n;i++){
+        for(i=0;i<n;i++){
             if((((i+1)%3) == 0) && (((i+1)%5) == 0)){
                 answer.push_back(a);
             }
