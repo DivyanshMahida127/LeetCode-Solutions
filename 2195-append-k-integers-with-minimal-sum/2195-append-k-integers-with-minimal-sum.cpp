7cf0d958abed5543e0ded4ffff346ca08d51c21c
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-08-05 10:39:19 +0530

    Time: 302 ms (52.84%), Space: 66.3 MB (51.44%) - LeetHub

---
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -0,0 +1,137 @@
+ class Solution {
+public:
+    long long minimalKSum(vector<int>& nums, int k) {
+        int n = nums.size(),i,j;
+        long long answer = 0,a,b,c;
+        long long remaining = k;
+        
+        if(n == 1){
+            if(nums[0] == 1){
+                c = (remaining+2)*(remaining+1);
+                c /= 2;
+                
+                answer = (c-1);
+                return answer;
+            }
+            else{
+                c = nums[0] - 1;
+                if(c == remaining){
+                    answer = c*(c+1);
+                    answer /= 2;
+                    // cout << "For c equals remaining" << "\n";
+                    return answer;
+                }
+                else if(c > remaining){
+                    answer = remaining*(remaining+1);
+                    answer /= 2;
+                    // cout << "c greater than remaining" << "\n";
+                    return answer;
+                }
+                else{
+                    answer = c*(c+1);
+                    answer /= 2;
+                    remaining -= c;
+                    b = nums[0];
+                    c = nums[0]+remaining;
+                    
+                    b = b*(b+1);
+                    b /= 2;
+                    c = c*(c+1);
+                    c /= 2;
+                    answer += (c-b);
+                    // cout << "c less than remaining" << "\n";
+                    return answer;
+                }
+            }
+        }
+        
+        sort(nums.begin(),nums.end());
+        if(nums[0] > 1){
+            a = nums[0]-1;
+            if(a <= remaining){
+                if(a%2 == 0){
+                    answer = a/2;
+                    answer *= (a+1);
+                }
+                else{
+                    answer = (a+1)/2;
+                    answer *= a;
+                }
+                remaining -= a;
+            }
+            else{
+                if(k%2 == 0){
+                    answer = k/2;
+                    answer *= (k+1);
+                }
+                else{
+                    answer = (k+1)/2;
+                    answer *= k;
+                }
+                remaining = 0;
+            }
+        }
+        
+        
+        if(remaining == 0){
+            return answer;
+        }
+        
+        j = 0;
+        
+        for(i=1;i<n;i++){
+            a = nums[i]-nums[j]-1;
+            if(a > 0){
+                if(a <= remaining){
+                    b = nums[i],c = nums[j];
+                    b = b*(b+1);
+                    b /= 2;
+                    
+                    c = c*(c+1);
+                    c /= 2;
+                    
+                    answer += (b - c - nums[i]);
+                    remaining -= a;
+                    // cout << remaining << " " << answer << "\n";
+                }
+                else{
+                    b = nums[j];
+                    c = b + remaining;
+                    
+                    b = b*(b+1);
+                    b /= 2;
+                    
+                    c = c*(c+1);
+                    c /= 2;
+                    
+                    answer += (c - b);
+                    remaining = 0;
+                    // cout << remaining << " " << answer << "\n";
+                }
+                
+                if(remaining <= 0){
+                    break;
+                }
+            }
+            if(remaining <= 0){
+                break;
+            }
+            
+            j++;
+        }
+        
+        if(remaining > 0){
+            b = nums[n-1];
+            c = b + remaining;
+            
+            b = b*(b+1);
+            b /= 2;
+
+            c = c*(c+1);
+            c /= 2;
+
+            answer += (c - b);
+        }
+        return answer;
+    }
+};
