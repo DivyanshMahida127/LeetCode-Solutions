2af49a9831c6cd47095d9f8805831578bfb53051
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-08-06 22:38:17 +0530

    Time: 314 ms (5.11%), Space: 33.7 MB (83.30%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    bool doitzero(vector<int> &nums,int i){
+        int j = i,k;
+        if(nums[j-1] == 0){
+            nums[j-1] = j;
+            return true;
+        }
+        else if(nums[j-1] == j){
+            return true;
+        }
+        else{
+            k = nums[j-1];
+            nums[j-1] = j;
+            return doitzero(nums,k);
+        }
+        return true;
+    }
+    
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         int n = nums.size(),i,j,k;
         
@@ -7,35 +24,26 @@ public:
             vector<int> answer;
             return answer;
         }
-        bool check = false;
-        while(check == false){
-            for(i=0;i<n;i++){
-                if((nums[i]-1) == i){
-                    continue;
-                }
-                if(nums[i] == 0){
-                    continue;
-                }
-                
-                j = nums[i]-1;
-                if(nums[j] == nums[i]){
-                    nums[i] = 0;
-                }
-                else{
-                    swap(nums[i],nums[j]);
-                }
+        
+        bool ans;
+        int c;
+        for(i=0;i<n;i++){
+            if(nums[i] == (i+1) || nums[i] == 0){
+                continue;
             }
-            
-            for(i=0;i<n;i++){
-                if(nums[i] == (i+1) || nums[i] == 0){
-                    check = true;
-                }
-                else{
-                    check = false;
-                    break;
+            else{
+                c = nums[i];
+                ans = doitzero(nums,c);
+                if(nums[i] == c){
+                    nums[i] = 0;
                 }
             }
         }
+        
+        for(i=0;i<n;i++){
+            cout << nums[i] << " ";
+        }
+        cout << "\n";
         
         vector<int> answer;
         for(i=0;i<n;i++){
