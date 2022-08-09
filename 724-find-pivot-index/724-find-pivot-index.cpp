875db368084df478ab9494c534164e7d620e2ad6
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-08-10 01:05:56 +0530

    Time: 35 ms (64.93%), Space: 31.1 MB (87.71%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int summation,answer,i,j,n;
+        int summation,answer,i,n;
         n = nums.size();
         answer = -1,summation = 0;
         for(i=0;i<n;i++){
@@ -24,14 +24,7 @@ public:
         if(summation == 0){
             return 0;
         }
-        // summation += nums[0];
-        // summation -= nums[n-1];
-        // if(summation == 0){
-        //     return n-1;
-        // }
-        // summation += nums[n-1];
-        // summation -= nums[0];
-        answer = -1;
+
         int tillnow = 0;
         for(i=1;i<n;i++){
             tillnow += nums[i-1];
