c760b1c1964715fc3a0595de10c945a588ac0de3
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-08-13 19:28:11 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.83%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        int i;
-        vector<vector<int>> answer(rowIndex+1);
-        answer[0].push_back(1);
+        int i,j,k,a;
+        vector<int> answer;
+        answer.push_back(1);
         
-        for(i=1;i<rowIndex+1;i++){
-            answer[i].push_back(1);
-            if(i != 1){
-                for(int j=0;j<answer[i-1].size()-1;j++){
-                    int x = answer[i-1][j] + answer[i-1][j+1];
-                    answer[i].push_back(x);
-                }
+        if(rowIndex == 0){
+            return answer;
+        }
+        else if(rowIndex == 1){
+            answer.push_back(1);
+            return answer;
+        }
+        answer.push_back(1);
+        for(i=2;i<rowIndex+1;i++){
+            a = answer[0];
+            for(j=1;j<answer.size();j++){
+                k = answer[j];
+                answer[j] = a+answer[j];
+                a = k;
             }
-            answer[i].push_back(1);
+            answer.push_back(1);
         }
         
-        vector<int> result;
-        for(i=0;i<answer[rowIndex].size();i++){
-            result.push_back(answer[rowIndex][i]);
-        }
-        return result;
+        return answer;
     }
 };
