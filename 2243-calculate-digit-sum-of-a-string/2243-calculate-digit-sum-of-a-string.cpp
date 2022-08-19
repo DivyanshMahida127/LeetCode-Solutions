f16789e5387d2de6e39bd809117097b053406b57
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-08-20 01:19:16 +0530

    Time: 5 ms (23.06%), Space: 6.2 MB (69.95%) - LeetHub

---
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    string digitSum(string s, int k) {
+        string answer,a,b;
+        int n = s.length();
+        int i,j,m,remainder;
+        bool done = false;
+        if(n <= k){
+            return s;
+        }
+        
+        
+        while(n > k){
+            a="";
+            j=0;
+            m=0;
+            for(i=0;i<n;i++){
+                done = false;
+                m = m + (s[i]-48);
+                j++;
+                if(j == k){
+                    done = true;
+                    b = "";
+                    if(m == 0){
+                        b = "0";
+                    }
+                    else{
+                        while(m != 0){
+                            remainder = m%10;
+                            b += (remainder+48);
+                            m /= 10;
+                        }
+                    }
+                    j=0;
+                    m=0;
+                    reverse(b.begin(),b.end());
+                    a += b;
+                }
+            }
+            // cout << a << "\n";
+            if(done == false){
+                if(m == 0){
+                    b = "0";
+                }
+                else{
+                    b = "";
+                    while(m != 0){
+                        remainder = m%10;
+                        b += (remainder+48);
+                        m /= 10;
+                    }
+                }
+                j=0;
+                m=0;
+                reverse(b.begin(),b.end());
+                a += b;
+            }
+            // cout << a << "\n";
+            s = a;
+            n = s.length();
+        }
+        
+        return s;
+    }
+};
