093c4b2eee3d7f869ab9d593970e5efdbe40e408
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-08-21 22:54:24 +0530

    Time: 14 ms (72.51%), Space: 11.9 MB (6.54%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -9,17 +9,14 @@ public:
         }
         long long int answer = 0;
         j = (nums[0]+nums[n-1])/2;
-        // cout << "j" << j << "\n";
         for(i=0;i<n;i++){
             answer += (max(nums[i],j)-min(nums[i],j));
-            // cout << answer << "\n";
         }
         
         long long int result = 0;
         j = nums[n/2];
         for(i=0;i<n;i++){
             result += (max(nums[i],j)-min(nums[i],j));
-            // cout << result << "\n";
         }
         int output = min(result,answer);
         return output;
