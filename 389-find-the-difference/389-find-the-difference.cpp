f38c62f2ccefd61afb22d5f6fb0f8ed7c8647216
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-09-15 14:36:57 +0530

    Time: 3 ms (81.03%), Space: 6.6 MB (67.61%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -4,27 +4,24 @@ public:
         int m,n;
         m = s.size(), n = t.size();
         char answer;
+        int a=0,b=0;
         int i,j;
-        vector<int> a(26,0),b(26,0);
         if(m == 0){
             answer = t[0];
             return answer;
         }
         else{
             for(i=0;i<m;i++){
-                a[s[i]-97]++;
+                a += (s[i]-97);
             }
             
             for(j=0;j<n;j++){
-                b[t[j]-97]++;
+                b += (t[j]-97);
             }
-            
-            for(i=0;i<26;i++){
-                if((a[i]+1) == (b[i])){
-                    answer = 97 + i;
-                    return answer;
-                }
-            }
+            // cout << a << " " << b << "\n";
+            int c = b-a;
+            // cout << c << "\n";
+            answer = c+97;
         }
         return answer;
     }
