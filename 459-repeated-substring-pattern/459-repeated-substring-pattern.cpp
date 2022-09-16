4709c1871dc483c7e09f6a7336f254351d5c7dcf
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-09-16 19:43:09 +0530

    Time: 151 ms (23.35%), Space: 24.4 MB (33.66%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -7,58 +7,43 @@ public:
         if(n == 1){
             return false;
         }
-        
-        // if(n%2 == 1){
-        //     for(i=1;i<n;i++){
-        //         if(s[i] != s[i-1]){
-        //             answer = false;
-        //             break;
-        //         }
-        //     }
-        //     return answer;
-        // }
-        // else{
-            answer = true;
-            for(i=1;i<n;i++){
-                if(s[i] != s[i-1]){
-                    answer = false;
-                    break;
+
+        answer = true;
+        for(i=1;i<n;i++){
+            if(s[i] != s[i-1]){
+                answer = false;
+                break;
+            }
+        }
+
+        if(answer == true){
+            return answer;
+        }
+
+        answer = false;
+        int count;
+        i=n/2;
+        while(i != 0){
+            count = 0;
+            if(n%i == 0){
+                string a,b;
+                a = s.substr(0,i);
+                count++;
+                j=i;
+                while(j < n){
+                    b = s.substr(j,i);
+                    if(b == a){
+                        count++;
+                    }
+                    j += i;
+                }
+
+                if(count == (n/i)){
+                    return true;
                 }
             }
-            
-            if(answer == true){
-                return answer;
-            }
-            
-            answer = false;
-            int count;
-            i=n/2;
-            while(i != 0){
-                count = 0;
-                // cout << "i " << i << "\n";
-                if(n%i == 0){
-                    string a,b;
-                    a = s.substr(0,i);
-                    count++;
-                    // cout << "a " << a << "\n";
-                    j=i;
-                    while(j < n){
-                        b = s.substr(j,i);
-                        // cout << "b " << b << "\n";
-                        if(b == a){
-                            count++;
-                        }
-                        j += i;
-                    }
-                    // cout << count << "\n";
-                    
-                    if(count == (n/i)){
-                        return true;
-                    }
-                }
-                i--;
-            }
-        // }
+            i--;
+        }
         return answer;
     }
 };
