dd66e45d4377ae7522ba16ba2566829a2dcc4be2
Divyansh Mahida <divyansh.mce19@sot.pdpu.ac.in>
2022-09-18 01:27:47 +0530

    Time: 30 ms (59.39%), Space: 23.2 MB (64.45%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    void count(vector<vector<int>>& grid,int i,int j, int& result,int m, int n){
+        i--;
+        if(i >= 0){
+            if(grid[i][j] == 1){
+                result++;
+                grid[i][j] = -1;
+                // cout << "ij: " << i << " " << j << "\n";
+                count(grid,i,j,result,m,n);
+            }
+        }
+        i++;
+        
+        i++;
+        if(i < m){
+            if(grid[i][j] == 1){
+                result++;
+                grid[i][j] = -1;
+                // cout << "ij: " << i << " " << j << "\n";
+                count(grid,i,j,result,m,n);
+            }
+        }
+        i--;
+        
+        j++;
+        if(j < n){
+            if(grid[i][j] == 1){
+                result++;
+                grid[i][j] = -1;
+                // cout << "ij: " << i << " " << j << "\n";
+                count(grid,i,j,result,m,n);
+            }
+        }
+        j--;
+        
+        j--;
+        if(j >= 0){
+            if(grid[i][j] == 1){
+                result++;
+                grid[i][j] = -1;
+                // cout << "ij: " << i << " " << j << "\n";
+                count(grid,i,j,result,m,n);
+            }   
+        }
+        j++;
+        
+        return ;
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m,n,i,j;
+        m = grid.size();
+        n = grid[0].size();
+        int answer = 0, result = 0;
+        
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                if(grid[i][j] == 1){
+                    grid[i][j] = -1;
+                    // cout << "ij: " << i << " " << j << "\n";
+                    result = 1;
+                    count(grid,i,j,result,m,n);
+                    // cout << "result " << result << "\n";
+                    answer = max(result,answer);
+                }
+            }
+        }
+        return answer;
+    }
+};
